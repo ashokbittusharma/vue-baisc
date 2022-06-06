51abd46bf0cd9833a40b4d18f80de1b0b9952dad
Your Name <you@example.com>
2022-06-06 17:37:54 +0530

    new vue setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      title : 'My ToDo App',
+      task : [
+        {id: 1, name: "vue version1", finished : false},
+        {id: 2, name: "vue version2", finished : false},
+        {id: 3, name: "vue version3", finished : false},
+      ]
+    }
+  }
+}
+</script>
